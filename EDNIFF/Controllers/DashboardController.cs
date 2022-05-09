e1e23cc91b7f609fdaa5f159111f8814b7e793f8
saudblaze <saudblaze@gmail.com>
2022-05-09 15:13:14 +0400

    = >

--- EDNIFF/Controllers/DashboardController.cs
+++ EDNIFF/Controllers/DashboardController.cs
@@ -3,6 +3,7 @@ using EDNIFF.Common;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Net;
@@ -31,13 +32,34 @@ namespace EDNIFF.Controllers
             String MachineName = Environment.MachineName;
 
 
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            process.StartInfo.FileName = "cmd.exe";
-            System.Diagnostics.Process.Start(@"/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal", @"~/Users/apple/Documents/myscript.sh");
-            process.StartInfo.Arguments = "system_profiler SPHardwareDataType";
-            process.Start();
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //process.StartInfo.FileName = "cmd.exe";
+            //System.Diagnostics.Process.Start(@"/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal", @"~/Users/apple/Documents/myscript.sh");
+            //process.StartInfo.Arguments = "system_profiler SPHardwareDataType";
+            //process.Start();
 
+
+            var command = @"~/Users/apple/Documents/myscript.sh";
+            var scriptFile = @"~/Users/apple/Documents/myscript.sh";//Path to shell script file
+            var arguments = string.Format("{0} {1} {2} {3} {4}", "testarg1", "testarg2", "testarg3", "testarg3", "testarg4");
+            var processInfo = new ProcessStartInfo()
+            {
+                FileName = command,
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+
+            };
+
+            Process process = Process.Start(processInfo);   // Start that process.
+            while (!process.StandardOutput.EndOfStream)
+            {
+                string result = process.StandardOutput.ReadLine();
+                // do something here
+            }
+            process.WaitForExit();
 
             //string Mac = string.Empty;
             //ManagementClass MC = new ManagementClass("system_profiler SPHardwareDataType");
