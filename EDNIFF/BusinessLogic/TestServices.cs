941dc2996614357b55aba87794c1c2dc1aa6f2ff
saudblaze <saudblaze@gmail.com>
2022-06-06 10:49:09 +0400

    = >

--- EDNIFF/BusinessLogic/TestServices.cs
+++ EDNIFF/BusinessLogic/TestServices.cs
@@ -1,11 +1,12 @@
 ﻿using EDNIFF.APIModels;
 using EDNIFF.Common;
+using EDNIFF.Helpers;
 using EDNIFF.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using static EDNIFF.Helpers.ConstantData;
+
 
 namespace EDNIFF.BusinessLogic
 {
@@ -18,74 +19,74 @@ namespace EDNIFF.BusinessLogic
                 vwSystemInfoDetail objParam = new vwSystemInfoDetail();
 
                 objParam.UnitId = 212297;
-                objParam.SerialNumber = "8C2H322";
+                objParam.SerialNumber = MacInfo.devices.Where(x => x.Category == ConstantData.Categories.Processor && x.DeviceName == ConstantData.DeviceNames.Processor).Select(x => x.Serial).FirstOrDefault(); 
                 objParam.InhouseSerialNo = "101601527962";
                 
 
 
 
                 var dict = new Dictionary<string, object>();
-                foreach (ReportProps prop in Enum.GetValues(typeof(ReportProps)))
-                {
-                    //switch (prop)
-                    //{
-                    //    case ReportProps.KeyboardLng:
-                    //        dict.Add(prop.ToString(), KeyboardLanguage);
-                    //        break;
-                    //    case ReportProps.MADEIN:
-                    //        dict.Add(prop.ToString(), Madein);
-                    //        break;
-                    //    case ReportProps.COA:
-                    //        dict.Add(prop.ToString(), OSCOA);
-                    //        break;
-                    //    case ReportProps.ObservNotes:
-                    //        dict.Add(prop.ToString(), ObservationComment);
-                    //        break;
-                    //    case ReportProps.UnitId:
-                    //        dict.Add(prop.ToString(), UnitId);
-                    //        break;
-                    //    case ReportProps.Grade:
-                    //        dict.Add(prop.ToString(), Grade);
-                    //        break;
-                    //    case ReportProps.BatteryBackup:
-                    //        if (!string.IsNullOrEmpty(batterybackupresult))
-                    //            dict.Add(prop.ToString(), batterybackupresult);
-                    //        break;
-                    //    //case ReportProps.RAM:
-                    //    //    dict.Add(prop.ToString(), UtilityHelper.GetDataSize(PublicVariables.RAMSize));
-                    //    //    break;
+                //foreach (ReportProps prop in Enum.GetValues(typeof(ReportProps)))
+                //{
+                //    //switch (prop)
+                //    //{
+                //    //    case ReportProps.KeyboardLng:
+                //    //        dict.Add(prop.ToString(), KeyboardLanguage);
+                //    //        break;
+                //    //    case ReportProps.MADEIN:
+                //    //        dict.Add(prop.ToString(), Madein);
+                //    //        break;
+                //    //    case ReportProps.COA:
+                //    //        dict.Add(prop.ToString(), OSCOA);
+                //    //        break;
+                //    //    case ReportProps.ObservNotes:
+                //    //        dict.Add(prop.ToString(), ObservationComment);
+                //    //        break;
+                //    //    case ReportProps.UnitId:
+                //    //        dict.Add(prop.ToString(), UnitId);
+                //    //        break;
+                //    //    case ReportProps.Grade:
+                //    //        dict.Add(prop.ToString(), Grade);
+                //    //        break;
+                //    //    case ReportProps.BatteryBackup:
+                //    //        if (!string.IsNullOrEmpty(batterybackupresult))
+                //    //            dict.Add(prop.ToString(), batterybackupresult);
+                //    //        break;
+                //    //    //case ReportProps.RAM:
+                //    //    //    dict.Add(prop.ToString(), UtilityHelper.GetDataSize(PublicVariables.RAMSize));
+                //    //    //    break;
 
-                    //    case ReportProps.PartNumber:
-                    //        dict.Add(prop.ToString(), PartNumber);
-                    //        break;
-                    //    case ReportProps.RAM:
-                    //        dict.Add(prop.ToString(), RamSize);
-                    //        break;
-                    //    case ReportProps.StorageType:
-                    //        dict.Add(prop.ToString(), DiskType);
-                    //        break;
-                    //    case ReportProps.StorageSize:
-                    //        dict.Add(prop.ToString(), DiskSize);
-                    //        break;
+                //    //    case ReportProps.PartNumber:
+                //    //        dict.Add(prop.ToString(), PartNumber);
+                //    //        break;
+                //    //    case ReportProps.RAM:
+                //    //        dict.Add(prop.ToString(), RamSize);
+                //    //        break;
+                //    //    case ReportProps.StorageType:
+                //    //        dict.Add(prop.ToString(), DiskType);
+                //    //        break;
+                //    //    case ReportProps.StorageSize:
+                //    //        dict.Add(prop.ToString(), DiskSize);
+                //    //        break;
 
-                    //    case ReportProps.Resolution:
-                    //        dict.Add(prop.ToString(), VideoRes);
-                    //        break;
-                    //    case ReportProps.DisplaySize:
-                    //        dict.Add(prop.ToString(), LCDSize);
-                    //        break;
+                //    //    case ReportProps.Resolution:
+                //    //        dict.Add(prop.ToString(), VideoRes);
+                //    //        break;
+                //    //    case ReportProps.DisplaySize:
+                //    //        dict.Add(prop.ToString(), LCDSize);
+                //    //        break;
 
-                    //    //case ReportProps.StorageHealth:
-                    //    //    dict.Add(prop.ToString(), UtilityHelper.GetDataSize(PublicVariables.RAMSize));
-                    //    //    break;
-                    //    default:
-                    //        dict.Add(prop.ToString(), system.getPropertyValue(result, prop));
-                    //        break;
-                    //}
+                //    //    //case ReportProps.StorageHealth:
+                //    //    //    dict.Add(prop.ToString(), UtilityHelper.GetDataSize(PublicVariables.RAMSize));
+                //    //    //    break;
+                //    //    default:
+                //    //        dict.Add(prop.ToString(), system.getPropertyValue(result, prop));
+                //    //        break;
+                //    //}
 
-                }
+                //}
 
-                dict.Add("NoOfPorts", PublicVariables.noofPorts);
+                //dict.Add("NoOfPorts", PublicVariables.noofPorts);
 
 
 
